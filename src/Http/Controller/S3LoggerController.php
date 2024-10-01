e7853199b1e3a079e167b8a374115b6e46bec36a
Nguyen Tung Bach <78024702+NguyenTungBach@users.noreply.github.com>
2024-10-01 15:14:02 +0700

    Update S3LoggerController.php update IAM for Controller

--- src/Http/Controller/S3LoggerController.php
+++ src/Http/Controller/S3LoggerController.php
@@ -15,15 +15,50 @@ class S3LoggerController
 
     public function __construct()
     {
+        $this->s3Client = new S3Client($this->configAwsSDK());
+    }
 
-        $this->s3Client = new S3Client([
-            'version' => config('s3logger.version'),
-            'region' => config('s3logger.region'),
-            'credentials' => [
-                'key' => config('s3logger.key'),
-                'secret' => config('s3logger.secret'),
-            ],
-        ]);
+    public function configAwsSDK()
+    {
+        $env = config('app.env');
+        if ($env !== 'local') {
+            $param = [
+                'version' => 'latest',
+                'region' => config('s3logger.region')
+            ];
+
+            if ($env === 'stage' || $env === 'product') { // case: access a server other than server 240
+                $stsClient = new StsClient($param);
+
+                // Assume IAM role atmtc để lấy temporary credentials
+                $assumeRoleResult = $stsClient->assumeRole([
+                    'RoleArn' => config('s3logger.roleArn'),
+                    'RoleSessionName' => config('s3logger.roleSessionName')
+                ]);
+
+                // Lấy temporary credentials từ AssumeRoleResult
+                $credentials = $assumeRoleResult['Credentials'];
+                $param = [
+                    'version' => 'latest',
+                    'region' => config('s3logger.region'),
+                    'credentials' => [
+                        'key' => $credentials['AccessKeyId'],
+                        'secret' => $credentials['SecretAccessKey'],
+                        'token' => $credentials['SessionToken']
+                    ]
+                ];
+            }
+        } else {
+            $param = [
+                'version' => config('s3logger.version'),
+                'region' => config('s3logger.region'),
+                'credentials' => [
+                    'key' => config('s3logger.key'),
+                    'secret' => config('s3logger.secret'),
+                ],
+            ];
+        }
+        return $param;
     }
 
     public function index()
