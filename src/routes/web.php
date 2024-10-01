b9250348b9dd06c0cf196cca30665cd50763c152
Nguyen Tung Bach <78024702+NguyenTungBach@users.noreply.github.com>
2024-10-01 10:36:24 +0700

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,4 +6,5 @@ Route::group(['prefix'=> 's3-logger', 'namespace' => 'VehoDev\S3Logger\Http\Cont
     Route::get('/logs/{log}', 'S3LoggerController@show')->name('s3logger.show');
     Route::get('/logs/view/{folder}/{fileName}', 'S3LoggerController@showLogFile')->name('s3logger.view');
     Route::get('/download/{folder}/{file}', 'S3LoggerController@download')->name('s3logger.download');
+    Route::get('/logs/synchronize/{folder}', 'S3LoggerController@synchronize')->name('s3logger.synchronize');
 });
