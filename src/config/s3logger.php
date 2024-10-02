2d6ffedada752f00aa31f0299318632d8c475c34
Nguyen Tung Bach <78024702+NguyenTungBach@users.noreply.github.com>
2024-10-02 16:04:30 +0700

    Update s3logger.php

--- src/config/s3logger.php
+++ src/config/s3logger.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'version' => env('AWS_SDK_VERSION', 'latest'),
-    'region' => env('AWS_DEFAULT_REGION', 'ap-northeast-1'),
-    'bucket' => env('AWS_S3_BUCKET', 'veho-log'),
-    'key' => env('AWS_ACCESS_KEY_ID'),
-    'secret' => env('AWS_SECRET_ACCESS_KEY'),
-    'roleArn' => env('AWS_Role_Arn'),
-    'roleSessionName' => env('AWS_Role_Session_Name'),
-    'assumeRole' => env('ASSUME_ROLE'),
+    'version' => env('LOG3_AWS_SDK_VERSION', 'latest'),
+    'region' => env('LOG3_AWS_DEFAULT_REGION', 'ap-northeast-1'),
+    'bucket' => env('LOG3_AWS_S3_BUCKET', 'veho-log'),
+    'key' => env('LOG3_AWS_ACCESS_KEY_ID'),
+    'secret' => env('LOG3_AWS_SECRET_ACCESS_KEY'),
+    'roleArn' => env('LOG3_AWS_Role_Arn'),
+    'roleSessionName' => env('LOG3_AWS_Role_Session_Name'),
+    'assumeRole' => env('LOG3_ASSUME_ROLE'),
 ];
