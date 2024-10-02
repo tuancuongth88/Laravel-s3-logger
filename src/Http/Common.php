87c69e38934ba5c96195f06d597f8bcd6e52cdd7
Nguyen Tung Bach <78024702+NguyenTungBach@users.noreply.github.com>
2024-10-02 15:22:41 +0700

    Create Common.php

---
+++ src/Http/Common.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace VehoDev\S3Logger\Http\Helpers;
+
+use Aws\S3\S3Client;
+use Aws\Sts\StsClient;
+
+class Common{
+    public static function configAwsSDK()
+    {
+        $checkAssumeRole = config('s3logger.assumeRole');
+        if (!$checkAssumeRole) {
+            $param = [
+                'version' => 'latest',
+                'region' => config('s3logger.region')
+            ];
+            $stsClient = new StsClient($param);
+
+            // Assume IAM role atmtc để lấy temporary credentials
+            $assumeRoleResult = $stsClient->assumeRole([
+                'RoleArn' => config('s3logger.roleArn'),
+                'RoleSessionName' => config('s3logger.roleSessionName')
+            ]);
+
+            // Lấy temporary credentials từ AssumeRoleResult
+            $credentials = $assumeRoleResult['Credentials'];
+            $param = [
+                'version' => 'latest',
+                'region' => config('s3logger.region'),
+                'credentials' => [
+                    'key' => $credentials['AccessKeyId'],
+                    'secret' => $credentials['SecretAccessKey'],
+                    'token' => $credentials['SessionToken']
+                ]
+            ];
+        } else {
+            $param = [
+                'version' => config('s3logger.version'),
+                'region' => config('s3logger.region'),
+                'credentials' => [
+                    'key' => config('s3logger.key'),
+                    'secret' => config('s3logger.secret'),
+                ],
+            ];
+        }
+        return $param;
+    }
+}
